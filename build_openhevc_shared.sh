27ed1dd515b9be22ae28e64a6ee35ec05fb2f2b2
buildworker-ubuntu64 <trac-gpac@telecom-paristech.fr>
2018-08-24 15:27:11 +0200

    add shared script for openhevc

---
+++ build_openhevc_shared.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+echo "Compilation of OpenHEVC"
+
+branch="ffmpeg_update"
+
+cd ./openHEVC/
+
+git checkout --force "$branch"
+git fetch
+git reset --hard FETCH_HEAD
+
+make clean
+./configure --disable-debug --disable-iconv
+make openhevc-shared || exit 1
+
+mkdir -p ../../gpac_public/bin/gcc
+if [ -f ./libopenhevc/libopenhevc.so ]; then
+	cp -af ./libopenhevc/libopenhevc.so* ../../gpac_public/bin/gcc
+fi
+if [ -f ./libopenhevc/libopenhevc.dylib ]; then
+        cp -af ./libopenhevc/libopenhevc*.dylib ../../gpac_public/bin/gcc
+fi
+cd ..
+
