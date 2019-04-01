a50e2f44e5a8d0fd93d42c6c3263596635cae690
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-04-01 14:51:21 +0200

    add openjp2 local

--- build_all.sh
+++ build_all.sh
@@ -2,10 +2,12 @@
 
 ARCH="linux"
 PLAT_ARCH="x86-unknown-linux"
+JP2=""
 
 if [ "$1" = "osx" ]; then
 	ARCH="osx"
 	PLAT_ARCH="universal-apple-macosx"
+	JP2="osx"
 fi
 
 
@@ -17,6 +19,7 @@ cd avcap
 ./BuildAndCopy2Public.sh "$ARCH"
 cd ..
 
+
 ./build_opensvc_static.sh "$ARCH"
 
 
@@ -27,3 +30,10 @@ cd js
 make -f Makefile.ref
 cp -av Linux_All_DBG.OBJ/libjs.a ../../gpac_public/extra_lib/lib/gcc/
 cd ..
+
+
+cd OpenJPEG
+make $JP2
+cp libopenjpeg.a ../../gpac_public/extra_lib/lib/gcc/
+cd ..
+
