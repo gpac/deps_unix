758ff75ebf6e016d85d5a8a8f417f6ead2c017e9
jeanlf <jeanlf@gpac.io>
2023-07-04 10:44:00 +0200

    added libcaption dep

--- build_all.sh
+++ build_all.sh
@@ -7,3 +7,5 @@ ARCH="$1"
 
 
 ./build_openhevc_static.sh "$ARCH"
+
+./build_libcaption_static.sh "$ARCH"
---
+++ build_libcaption_static.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+echo "Compilation of libcaption"
+
+branch="master"
+
+#git submodule update --init
+
+cd ./libcaption/
+
+git checkout --force "$branch"
+git fetch
+git reset --hard FETCH_HEAD
+
+make clean
+cmake -DENABLE_RE2C=OFF .
+make || exit 1
+
+
+mkdir -p ../../gpac_public/extra_lib/include/caption
+cp ./caption/*.h ../../gpac_public/extra_lib/include/caption/
+
+mkdir -p ../../gpac_public/extra_lib/lib/gcc
+cp ./libcaption.a ../../gpac_public/extra_lib/lib/gcc
+cd ..
